3fb90ac0d8d837f40a7f35fdcb560fc37e453129
shirokikhdo <shirokikhdo@gmail.com>
2024-12-16 23:37:07 +0300

    Обработка запроса на вход пользователя в систему

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Api.Common;
 using Api.Data;
 using Api.ModelDto;
 using Api.Models;
+using Api.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,15 +14,18 @@ public class AuthController : StoreController
 {
     private readonly UserManager<AppUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly JwtTokenGenerator _jwtTokenGenerator;
 
     public AuthController(
         AppDbContext dbContext,
         UserManager<AppUser> userManager,
-        RoleManager<IdentityRole> roleManager) 
+        RoleManager<IdentityRole> roleManager,
+        JwtTokenGenerator jwtTokenGenerator) 
         : base(dbContext)
     {
         _userManager = userManager;
         _roleManager = roleManager;
+        _jwtTokenGenerator = jwtTokenGenerator;
     }
 
     [HttpPost]
@@ -80,6 +84,38 @@ public class AuthController : StoreController
         {
             StatusCode = HttpStatusCode.OK,
             Result = "Регистрация завершена"
+        });
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ResponseServer>> Login(
+        [FromBody] LoginRequestDto loginRequestDto)
+    {
+        var user = await _dbContext.AppUsers
+            .FirstOrDefaultAsync(x=>string.Equals(
+                x.Email.ToLower(), 
+                loginRequestDto.Email.ToLower()));
+
+        if(user is null 
+           || !await _userManager.CheckPasswordAsync(user, loginRequestDto.Password))
+            return BadRequest(new ResponseServer
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Такого пользователя нет" }
+            });
+
+        var roles = await _userManager.GetRolesAsync(user);
+        var token = _jwtTokenGenerator.GenerateJwtToken(user, roles);
+
+        return Ok(new ResponseServer
+        {
+            StatusCode = HttpStatusCode.OK,
+            Result = new LoginResponseDto
+            {
+                Email = user.Email,
+                Token = token
+            }
         });
     }
 }

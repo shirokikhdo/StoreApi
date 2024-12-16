d06c3a3646f07cea6d71f4cc3215ad94f2df4205
shirokikhdo <shirokikhdo@gmail.com>
2024-12-16 22:36:33 +0300

    Endpoint регистрации пользователя

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,6 +1,11 @@
-﻿using Api.Data;
+﻿using System.Net;
+using Api.Common;
+using Api.Data;
+using Api.ModelDto;
 using Api.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers;
 
@@ -17,5 +22,64 @@ public class AuthController : StoreController
     {
         _userManager = userManager;
         _roleManager = roleManager;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ResponseServer>> Register(
+        [FromBody] RegisterRequestDto registerRequestDto)
+    {
+        if (registerRequestDto is null)
+            return BadRequest(new ResponseServer
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = {"Некорректная модель запроса"}
+            });
+
+        var user = await _dbContext.AppUsers.FirstOrDefaultAsync(
+            x => string.Equals(
+                x.UserName.ToLower(), 
+                registerRequestDto.Username.ToLower()));
+
+        if(user != null)
+            return BadRequest(new ResponseServer
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Такой пользователь есть" }
+            });
+
+        var newUser = new AppUser
+        {
+            UserName = registerRequestDto.Username,
+            Email = registerRequestDto.Email,
+            NormalizedEmail = registerRequestDto.Email.ToUpper(),
+            FirstName = registerRequestDto.Username
+        };
+
+        var userResult = await _userManager.CreateAsync(
+            newUser, 
+            registerRequestDto.Password);
+
+        if(!userResult.Succeeded)
+            return BadRequest(new ResponseServer
+            {
+                IsSuccess = false,
+                StatusCode = HttpStatusCode.BadRequest,
+                ErrorMessages = { "Ошибка регистрации" }
+            });
+
+        var newUserRole = registerRequestDto.Role.Equals(
+            SharedData.Roles.Admin, StringComparison.OrdinalIgnoreCase)
+            ? SharedData.Roles.Admin
+            : SharedData.Roles.Consumer;
+
+        await _userManager.AddToRoleAsync(newUser, newUserRole);
+
+        return Ok(new ResponseServer
+        {
+            StatusCode = HttpStatusCode.OK,
+            Result = "Регистрация завершена"
+        });
     }
 }

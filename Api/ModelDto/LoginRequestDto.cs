d930a9e70d2d5ba3e0b417a0acd9f0a6e6a221c0
shirokikhdo <shirokikhdo@gmail.com>
2024-12-16 23:19:15 +0300

    DTO-модели для входа в систему

---
+++ Api/ModelDto/LoginRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.ModelDto;
+
+public class LoginRequestDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
---
+++ Api/ModelDto/LoginResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.ModelDto;
+
+public class LoginResponseDto
+{
+    public string Email { get; set; }
+    public string Token { get; set; }
+}

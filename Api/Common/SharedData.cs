5a07158301f61d167b38d65e87034e1343fbbd5e
shirokikhdo <shirokikhdo@gmail.com>
2024-12-17 23:31:23 +0300

    Статусы заказов

--- Api/Common/SharedData.cs
+++ Api/Common/SharedData.cs
@@ -10,4 +10,14 @@ public static class SharedData
         public static IReadOnlyList<string> AllRoles => 
             new List<string> {Admin, Consumer};
     }
+
+    public static class OrderStatus
+    {
+        public const string Pending = "pending";
+        public const string ReadyToSheep = "ready-to-sheep";
+        public const string Completed = "completed";
+
+        public static IReadOnlyList<string> AllStatus =>
+            new List<string> { Pending, ReadyToSheep, Completed };
+    }
 }

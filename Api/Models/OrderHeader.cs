cbc5855f0370c04b88fdf8c47fca7381c09af001
shirokikhdo <shirokikhdo@gmail.com>
2024-12-17 23:28:03 +0300

    Модель всего заказа

---
+++ Api/Models/OrderHeader.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Models;
+
+public class OrderHeader
+{
+    [Key]
+    public int OrderHeaderId { get; set; }
+    [Required]
+    public string CustomerName { get; set; }
+    [Required]
+    public string CustomerEmail { get; set; }
+    public string AppUserId { get; set; }
+    [ForeignKey("AppUserId")]
+    public AppUser User { get; set; }
+    public double TotalOrderAmount { get; set; }
+    public DateTime OrderDateTime { get; set; }
+    public string Status { get; set; }
+    public int TotalCount { get; set; }
+    public IEnumerable<OrderDetails> OrderDetailItems { get; set; }
+}

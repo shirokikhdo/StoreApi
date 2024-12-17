2c618c8d8d2d03402ecc76d743b8e37fb7a23d29
shirokikhdo <shirokikhdo@gmail.com>
2024-12-17 23:37:30 +0300

    Контроллер создания заказов

---
+++ Api/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Api.Data;
+using Api.Services;
+
+namespace Api.Controllers;
+
+public class OrderController : StoreController
+{
+    private readonly OrdersService _ordersService;
+
+    public OrderController(
+        AppDbContext dbContext,
+        OrdersService ordersService) 
+        : base(dbContext)
+    {
+        _ordersService = ordersService;
+    }
+}

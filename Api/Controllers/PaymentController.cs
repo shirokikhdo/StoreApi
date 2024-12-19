25d7905229fb08539f7d63ddc75ae53408548627
shirokikhdo <shirokikhdo@gmail.com>
2024-12-19 22:59:02 +0300

    Контроллер оплаты заказов

---
+++ Api/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using Api.Data;
+using Api.Models;
+using Api.Services.Payment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class PaymentController : StoreController
+{
+    private readonly IPaymentService _paymentService;
+
+    public PaymentController(
+        AppDbContext dbContext,
+        IPaymentService paymentService) 
+        : base(dbContext)
+    {
+        _paymentService = paymentService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ResponseServer>> MakePayment(
+        string userId, int orderHeaderId, string cardNumber) =>
+        await _paymentService.HandlePaymentAsync(userId, orderHeaderId, cardNumber);
+}
